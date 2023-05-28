8f4704936cf4b2398b006c24fc0531fbc55d0c10
ASSE Romain <asse-romain@outlook.fr>
2023-05-28 11:00:53 +0200

    Ajout du fichier d'entrée, implémentation d'un 'Hello, Word'

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * ASSE Romain FERRE Cedric, PROCACCIA Paul, LEQUEUX Alexis
+ * tp ricochet robots
+ *
+ * Point d'entré du programme
+ */
+
+/******* Native include *******/
+#include <iostream>
+
+/******* Project include ******/
+
+/******* Espace de nommage ****/
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!" << endl;
+    return 0;
+}
